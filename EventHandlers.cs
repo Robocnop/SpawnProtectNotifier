6df342fc723b78885b880555642e40f8a022fec1
Robocnop <robocnop@gmail.com>
2024-06-23 17:13:44 +0200

    Push my stuff

---
+++ EventHandlers.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+using MEC;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+namespace SpawnProtectNotifier
+{
+    public class EventHandlers
+    {
+        public void OnSpawning(SpawningEventArgs ev)
+        {
+            if (ev.Player.Role.Type != RoleTypeId.Spectator && ev.Player.Role.Type != RoleTypeId.None)
+            {
+                // Délai avant que la protection de spawn soit désactivée
+                Timing.CallDelayed(10f, () =>
+                {
+                    if (!ev.Player.IsCuffed && ev.Player.Role.Type != RoleTypeId.Spectator && ev.Player.Role.Type != RoleTypeId.None)
+                    {
+                        Map.Broadcast(10, $"La protection de spawn pour {ev.Player.Nickname} n'est plus active.");
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using System;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace SpawnProtectNotifier
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "SpawnProtectNotifier";
+        public override string Prefix { get; } = "spawn_protect_notifier";
+        public override string Author { get; } = "Robocnop";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 4, 3);
+
+        public static Plugin Instance { get; private set; }
+
+        private EventHandlers eventHandlers;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            eventHandlers = new EventHandlers();
+
+            Player.Spawning += eventHandlers.OnSpawning;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Spawning -= eventHandlers.OnSpawning;
+
+            eventHandlers = null;
+            Instance = null;
+
+            base.OnDisabled();
+        }
+    }
+}
